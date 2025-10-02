2a543ea30366d74d449f3f4ae0ca4f1ab92cbae0
NhProGamer <neo.huyghe@gmail.com>
2025-10-02 10:36:12 +0200

    Vote struct created

---
+++ internal/domain/entities/vote.go
@@ -0,0 +1,25 @@
+package entities
+
+type VoteType string
+type VoteStatus string
+
+type Vote struct {
+	ID     string
+	Type   VoteType
+	Status VoteStatus
+
+	// Intégrer une notion de temps de vote, démarrage, fermeture etc...
+	EligibleVoters  []PlayerID
+	EligibleTargets []PlayerID
+	Ballots         map[PlayerID]*PlayerID
+	AllowAbstain    bool
+
+	Result *VoteResult
+}
+
+type VoteResult struct {
+	Target      *PlayerID
+	Counts      map[PlayerID]int
+	IsTie       bool
+	TiedPlayers []PlayerID
+}
